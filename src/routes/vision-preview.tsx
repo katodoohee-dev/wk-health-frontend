850f386d6d2fdf6f6c17f32dbdf442528879672c
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 02:49:59 +0700

    Add Vision preview reference state

---
+++ src/routes/vision-preview.tsx
@@ -0,0 +1,3 @@
+import { createFileRoute } from '@tanstack/react-router';
+import { Vision } from '@/components/wk-design';
+export const Route=createFileRoute('/vision-preview')({component:Vision});
