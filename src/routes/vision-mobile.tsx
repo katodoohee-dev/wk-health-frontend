aae39555adb451869adeb4a2707dc6c084b6fde9
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 02:51:09 +0700

    Use dedicated mobile chrome for Vision reference screen

--- src/routes/vision-mobile.tsx
+++ src/routes/vision-mobile.tsx
@@ -1,3 +1,3 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { Vision } from '@/components/wk-design';
-export const Route=createFileRoute('/vision-mobile')({component:Vision});
+import { MobileVision } from '@/components/wk-mobile-variants';
+export const Route=createFileRoute('/vision-mobile')({component:MobileVision});
