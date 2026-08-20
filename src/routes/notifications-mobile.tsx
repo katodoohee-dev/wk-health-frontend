3bd1709a9fa94d1382897eafc63fcdd1aa9c0060
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 02:49:40 +0700

    Add notifications mobile reference route

---
+++ src/routes/notifications-mobile.tsx
@@ -0,0 +1,3 @@
+import { createFileRoute } from '@tanstack/react-router';
+import { Notifications } from '@/components/wk-design';
+export const Route=createFileRoute('/notifications-mobile')({component:Notifications});
