377bb210ce40a9d87d21284cdd5141cdc6268eb2
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 02:51:16 +0700

    Use mobile chrome for Vision preview reference state

--- src/routes/vision-preview.tsx
+++ src/routes/vision-preview.tsx
@@ -1,3 +1,3 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { Vision } from '@/components/wk-design';
-export const Route=createFileRoute('/vision-preview')({component:Vision});
+import { MobileVision } from '@/components/wk-mobile-variants';
+export const Route=createFileRoute('/vision-preview')({component:MobileVision});
