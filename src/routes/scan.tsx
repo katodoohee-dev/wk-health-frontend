7583dfabec04c07864f3023d963af491c23bca40
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 03:51:50 +0700

    Use live scan capture on desktop and mobile

--- src/routes/scan.tsx
+++ src/routes/scan.tsx
@@ -1,4 +1,4 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Scan as DesktopScan } from "@/components/wk-design";
+import { LiveScanPage } from "@/components/wk/live-scan-page";
 import { MobileScan } from "@/components/wk/mobile-scan";
-export const Route=createFileRoute("/scan")({component:()=> <><div className="hidden lg:block"><DesktopScan/></div><MobileScan/></>});
+export const Route=createFileRoute("/scan")({component:()=> <><div className="hidden lg:block"><LiveScanPage/></div><div className="lg:hidden"><MobileScan/></div></>});
