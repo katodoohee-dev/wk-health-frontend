08943a4741efa8f675818565e9d781a8176affe7
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 03:30:36 +0700

    Add You screen from mobile reference

---
+++ src/routes/you.tsx
@@ -0,0 +1,3 @@
+import { createFileRoute } from '@tanstack/react-router';
+import { MobileYou } from '@/components/wk/mobile-pages';
+export const Route=createFileRoute('/you')({component:MobileYou});
