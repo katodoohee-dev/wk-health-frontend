0a26ac905c849071d5e3054f8e76eef67a73752c
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 02:49:32 +0700

    Add mobile reference screen route

---
+++ src/routes/health-mobile.tsx
@@ -0,0 +1,3 @@
+import { createFileRoute } from '@tanstack/react-router';
+import { Health } from '@/components/wk-design';
+export const Route=createFileRoute('/health-mobile')({component:Health});
