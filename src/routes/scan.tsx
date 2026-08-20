978643112311694c857dc93fe0a2667575eed465
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 03:40:39 +0700

    Normalize scan route declaration for production contract

--- src/routes/scan.tsx
+++ src/routes/scan.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from '@tanstack/react-router';
-import { Scan as DesktopScan } from '@/components/wk-design';
-import { MobileScan } from '@/components/wk/mobile-scan';
-export const Route=createFileRoute('/scan')({component:()=> <><div className="hidden lg:block"><DesktopScan/></div><MobileScan/></>});
+import { createFileRoute } from "@tanstack/react-router";
+import { Scan as DesktopScan } from "@/components/wk-design";
+import { MobileScan } from "@/components/wk/mobile-scan";
+export const Route=createFileRoute("/scan")({component:()=> <><div className="hidden lg:block"><DesktopScan/></div><MobileScan/></>});
