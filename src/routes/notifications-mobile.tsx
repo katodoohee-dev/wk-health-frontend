edc3e56c288051c0c537948e2081b960051bf767
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 03:33:05 +0700

    Remove obsolete notification duplicate route

--- src/routes/notifications-mobile.tsx
+++
@@ -1,3 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-import { Notifications } from '@/components/wk-design';
-export const Route=createFileRoute('/notifications-mobile')({component:Notifications});
