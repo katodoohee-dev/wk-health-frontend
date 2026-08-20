4413ce245b9dd5c8f14d120e05ef64d7c216134e
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 03:30:31 +0700

    Add sound control screen from recording

---
+++ src/routes/sound.tsx
@@ -0,0 +1,3 @@
+import { createFileRoute } from '@tanstack/react-router';
+import { MobileSound } from '@/components/wk/mobile-pages';
+export const Route=createFileRoute('/sound')({component:MobileSound});
