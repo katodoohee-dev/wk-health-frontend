8b441110d4a47251fe54348bfcaba70d61e31436
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 03:41:02 +0700

    Normalize hidden production insight route declaration

--- src/routes/insight.tsx
+++ src/routes/insight.tsx
@@ -1,3 +1,3 @@
-import { createFileRoute } from '@tanstack/react-router';
-import { MobileOverview } from '@/components/wk/mobile-pages';
-export const Route=createFileRoute('/insight')({component:MobileOverview});
+import { createFileRoute } from "@tanstack/react-router";
+import { MobileOverview } from "@/components/wk/mobile-pages";
+export const Route=createFileRoute("/insight")({component:MobileOverview});
