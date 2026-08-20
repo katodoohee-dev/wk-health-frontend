ef26f384ca62d1a1f819205dc3a6665daea9f487
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 03:32:58 +0700

    Remove obsolete route without source design

--- src/routes/health-mobile.tsx
+++
@@ -1,3 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-import { MobileHealth } from '@/components/wk-mobile-variants';
-export const Route=createFileRoute('/health-mobile')({component:MobileHealth});
