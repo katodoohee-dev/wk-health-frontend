cc55736705d8d1f935150eb798fc46f84c20a48d
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 02:49:52 +0700

    Add mobile Vision reference route

---
+++ src/routes/vision-mobile.tsx
@@ -0,0 +1,3 @@
+import { createFileRoute } from '@tanstack/react-router';
+import { Vision } from '@/components/wk-design';
+export const Route=createFileRoute('/vision-mobile')({component:Vision});
