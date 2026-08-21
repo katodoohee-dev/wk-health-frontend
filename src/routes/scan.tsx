0707cc4fdc92b3fb7686b9b65e424fb30a4404a8
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 18:34:04 +0700

    Wire Scan route to complete capture prototype

--- src/routes/scan.tsx
+++ src/routes/scan.tsx
@@ -1,4 +1,3 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { LiveScanPage } from "@/components/wk/live-scan-page";
-import { MobileScan } from "@/components/wk/mobile-scan";
-export const Route=createFileRoute("/scan")({component:()=> <><div className="hidden lg:block"><LiveScanPage/></div><div className="lg:hidden"><MobileScan/></div></>});
+import { createFileRoute } from '@tanstack/react-router';
+import { PrototypeScreen } from '@/components/wk/prototype-screens';
+export const Route=createFileRoute('/scan')({component:()=> <PrototypeScreen page="scan"/>});
