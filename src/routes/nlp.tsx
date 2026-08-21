9b78c92be39af9370581ce49df5bf664d66130a1
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 20:27:50 +0700

    Use live backend data for NLP

--- src/routes/nlp.tsx
+++ src/routes/nlp.tsx
@@ -1,3 +1,3 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { PrototypeScreen } from '@/components/wk/prototype-screens';
-export const Route=createFileRoute('/nlp')({component:()=> <PrototypeScreen page="nlp"/>});
+import { LiveDataPage } from '@/components/wk/live-data-page';
+export const Route=createFileRoute('/nlp')({component:()=> <LiveDataPage page="nlp"/>});
