da9d04914b7c13c6d1edf1d49871d81cf3db5ffa
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 15:26:44 +0700

    Use supplied Vision UI desktop and mobile states

--- src/routes/vision.tsx
+++ src/routes/vision.tsx
@@ -1,3 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { Vision } from '@/components/wk-design';
-export const Route=createFileRoute('/vision')({component:()=> <Vision/>});
+import { MobileVision } from '@/components/wk/vision-mobile-extra';
+export const Route=createFileRoute('/vision')({component:()=> <><div className="hidden lg:block"><Vision/></div><div className="lg:hidden"><MobileVision/></div></>});
