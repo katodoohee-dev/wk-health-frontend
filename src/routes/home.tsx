c93e42d48430223eea93c3de92ecbb91a8a33fbc
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 20:27:02 +0700

    Use live backend data for home

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,3 +1,3 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { PrototypeScreen } from '@/components/wk/prototype-screens';
-export const Route=createFileRoute('/home')({component:()=> <PrototypeScreen page="home"/>});
+import { LiveDataPage } from '@/components/wk/live-data-page';
+export const Route=createFileRoute('/home')({component:()=> <LiveDataPage page="home"/>});
