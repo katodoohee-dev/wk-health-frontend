05c6e14095b070b02636f0efb3cdc5a88513223d
katodoohee-dev <katodoohee@gmail.com>
2026-08-22 01:00:22 +0700

    Restore canonical WK vision design

--- src/routes/vision.tsx
+++ src/routes/vision.tsx
@@ -1,3 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { LiveDataPage } from '@/components/wk/live-data-page';
-export const Route=createFileRoute('/vision')({component:()=> <LiveDataPage page="vision"/>});
+import { Vision } from '@/components/wk-design';
+
+export const Route = createFileRoute('/vision')({ component: Vision });
