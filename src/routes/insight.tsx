63844e391902770fa91641e8b6b3bfcfe70b2e03
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 15:28:31 +0700

    Remove route not present in supplied design sources

--- src/routes/insight.tsx
+++
@@ -1,3 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { MobileOverview } from "@/components/wk/mobile-pages";
-export const Route=createFileRoute("/insight")({component:MobileOverview});
