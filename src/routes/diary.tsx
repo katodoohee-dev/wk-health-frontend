5a774ad1076b71e4d1044836303a3cd253aaecd4
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 20:27:09 +0700

    Use live backend data for diary

--- src/routes/diary.tsx
+++ src/routes/diary.tsx
@@ -1,3 +1,3 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { PrototypeScreen } from '@/components/wk/prototype-screens';
-export const Route=createFileRoute('/diary')({component:()=> <PrototypeScreen page="diary"/>});
+import { LiveDataPage } from '@/components/wk/live-data-page';
+export const Route=createFileRoute('/diary')({component:()=> <LiveDataPage page="diary"/>});
