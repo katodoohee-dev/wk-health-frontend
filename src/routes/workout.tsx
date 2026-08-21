4b09de0d4aaf3dd332e7c3099d2eea36bd670882
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 18:34:38 +0700

    Wire Workout route to complete session prototype

--- src/routes/workout.tsx
+++ src/routes/workout.tsx
@@ -1,3 +1,3 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { MobileWorkout } from '@/components/wk/mobile-pages';
-export const Route=createFileRoute('/workout')({component:MobileWorkout});
+import { PrototypeScreen } from '@/components/wk/prototype-screens';
+export const Route=createFileRoute('/workout')({component:()=> <PrototypeScreen page="workout"/>});
