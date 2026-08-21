35a94ccecf1b4503c9000f06900d842e5c4defc4
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 18:33:31 +0700

    Wire Music route to living player prototype

--- src/routes/music.tsx
+++ src/routes/music.tsx
@@ -1,4 +1,3 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { Music as DesktopMusic } from '@/components/wk-design';
-import { MobileMusic } from '@/components/wk/mobile-pages';
-export const Route=createFileRoute('/music')({component:()=> <><div className="hidden lg:block"><DesktopMusic/></div><MobileMusic/></>});
+import { PrototypeScreen } from '@/components/wk/prototype-screens';
+export const Route=createFileRoute('/music')({component:()=> <PrototypeScreen page="music"/>});
