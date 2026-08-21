fd9531d97a9ce15cc687922e8424ac6a1afad489
katodoohee-dev <katodoohee@gmail.com>
2026-08-22 00:59:40 +0700

    Restore canonical WK notifications design

--- src/routes/notifications.tsx
+++ src/routes/notifications.tsx
@@ -1,3 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { LiveDataPage } from '@/components/wk/live-data-page';
-export const Route=createFileRoute('/notifications')({component:()=> <LiveDataPage page="notifications"/>});
+import { Notifications } from '@/components/wk-design';
+
+export const Route = createFileRoute('/notifications')({ component: Notifications });
