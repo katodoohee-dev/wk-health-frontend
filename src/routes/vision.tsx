fcf6e5ae5b843c13d19dbb734a047e1538918412
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 20:28:23 +0700

    Use live backend data for vision insights

--- src/routes/vision.tsx
+++ src/routes/vision.tsx
@@ -1,3 +1,3 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { PrototypeScreen } from '@/components/wk/prototype-screens';
-export const Route=createFileRoute('/vision')({component:()=> <PrototypeScreen page="vision"/>});
+import { LiveDataPage } from '@/components/wk/live-data-page';
+export const Route=createFileRoute('/vision')({component:()=> <LiveDataPage page="vision"/>});
