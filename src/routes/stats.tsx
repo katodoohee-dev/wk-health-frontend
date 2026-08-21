3ff682b478ffca5cdad286376e77b31aaa99a6c5
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 18:34:22 +0700

    Wire Stats route to complete trend prototype

--- src/routes/stats.tsx
+++ src/routes/stats.tsx
@@ -1,4 +1,3 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { VisionStats } from '@/components/wk/vision-stats';
-import { MobileStats } from '@/components/wk/mobile-pages';
-export const Route=createFileRoute('/stats')({component:()=> <><div className="hidden lg:block"><VisionStats/></div><div className="lg:hidden"><MobileStats/></div></>});
+import { PrototypeScreen } from '@/components/wk/prototype-screens';
+export const Route=createFileRoute('/stats')({component:()=> <PrototypeScreen page="stats"/>});
