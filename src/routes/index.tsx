0421f9714c2bd6cd59010d8bb836d90175222aa4
katodoohee-dev <katodoohee@gmail.com>
2026-08-22 00:59:00 +0700

    Restore canonical WK health overview design with charts

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { LiveDataPage } from '@/components/wk/live-data-page';
-export const Route=createFileRoute('/')({component:()=> <LiveDataPage page="home"/>});
+import { Health } from '@/components/wk-design';
+
+export const Route = createFileRoute('/')({ component: Health });
