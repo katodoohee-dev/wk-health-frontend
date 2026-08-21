368c6b3b30b21c92ba89c0f044af7d5637635a50
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 20:24:50 +0700

    Use live backend-backed sound control screen

--- src/routes/sound-control.tsx
+++ src/routes/sound-control.tsx
@@ -1,3 +1,3 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { PrototypeScreen } from '@/components/wk/prototype-screens';
-export const Route=createFileRoute('/sound-control')({component:()=> <PrototypeScreen page="sound-control"/>});
+import { LiveSoundControl } from '@/components/wk/live-sound-control';
+export const Route=createFileRoute('/sound-control')({component:LiveSoundControl});
