818db41a458fedfa046438e20ecaa0277f548b54
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 18:33:25 +0700

    Wire Mood route to complete interactive prototype

--- src/routes/mood.tsx
+++ src/routes/mood.tsx
@@ -1,3 +1,3 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { MobileMood } from '@/components/wk/mobile-pages';
-export const Route=createFileRoute('/mood')({component:MobileMood});
+import { PrototypeScreen } from '@/components/wk/prototype-screens';
+export const Route=createFileRoute('/mood')({component:()=> <PrototypeScreen page="mood"/>});
