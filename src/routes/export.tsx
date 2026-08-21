23095ac2294e820c03384bb5617f503b41b3ca75
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 20:27:23 +0700

    Use live backend data for export

--- src/routes/export.tsx
+++ src/routes/export.tsx
@@ -1,3 +1,3 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { PrototypeScreen } from '@/components/wk/prototype-screens';
-export const Route=createFileRoute('/export')({component:()=> <PrototypeScreen page="export"/>});
+import { LiveDataPage } from '@/components/wk/live-data-page';
+export const Route=createFileRoute('/export')({component:()=> <LiveDataPage page="export"/>});
