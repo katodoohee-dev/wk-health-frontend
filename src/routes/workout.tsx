808e9e2226ac95b301facce01a3a674624dd2f64
katodoohee-dev <katodoohee@gmail.com>
2026-08-21 20:28:31 +0700

    Use live backend data for workout

--- src/routes/workout.tsx
+++ src/routes/workout.tsx
@@ -1,3 +1,3 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { PrototypeScreen } from '@/components/wk/prototype-screens';
-export const Route=createFileRoute('/workout')({component:()=> <PrototypeScreen page="workout"/>});
+import { LiveDataPage } from '@/components/wk/live-data-page';
+export const Route=createFileRoute('/workout')({component:()=> <LiveDataPage page="workout"/>});
