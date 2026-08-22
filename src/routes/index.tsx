71917e1d119b13bcced3f1e640e85390e3ac2a50
katodoohee-dev <katodoohee@gmail.com>
2026-08-22 20:53:26 +0700

    restore(ui): use canonical original overview on root route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { LiveHealthOverview } from '@/components/wk/live-design-pages';
+import { VisionOverview } from '@/components/wk/vision-overview';
 
-export const Route = createFileRoute('/')({ component: LiveHealthOverview });
+export const Route = createFileRoute('/')({ component: VisionOverview });
